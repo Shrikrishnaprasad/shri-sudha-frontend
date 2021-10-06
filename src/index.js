9442e910bfa9aec60fc8e4d6d58138c116868630
Shrikrishnaprasad <sgkrishna619@gmail.com>
2021-10-06 08:56:40 +0530

    contextApi

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import { Router } from "react-router-dom";
 
 import "assets/scss/material-kit-react.scss?v=1.10.0";
 
-// pages for this product
-import LandingPage from "views/LandingPage/LandingPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
-import RegisterPage from "views/RegisterPage/RegisterPage";
-import SubscribePage from "views/SubscribePage/SubscribePage";
+import { AppProvider } from "components/ContextApi/context";
+import App from "components/App/App";
 
 var hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Route exact path="/contact" component={ProfilePage} />
-      <Route exact path="/login" component={LoginPage} />
-      <Route exact path="/register" component={RegisterPage} />
-      <Route exact path="/subscribe" component={SubscribePage} />
-    </Switch>
-  </Router>,
+  <AppProvider>
+    <Router history={hist}>
+      <App />
+    </Router>
+  </AppProvider>,
   document.getElementById("root")
 );
